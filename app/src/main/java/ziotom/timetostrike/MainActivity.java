5081512754100dce25d3b751cdd740aa5a148430
sebaphil <seba.fil@live.com>
2017-04-21 15:08:22 +0200

    Implemented BasicAdapter

--- app/src/main/java/ziotom/timetostrike/MainActivity.java
+++ app/src/main/java/ziotom/timetostrike/MainActivity.java
@@ -11,6 +11,8 @@ import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -24,6 +26,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +39,29 @@ public class MainActivity extends AppCompatActivity {
 
         final LinearLayout mainActivityLayout = new LinearLayout(this);
         mainActivityLayout.setOrientation(LinearLayout.VERTICAL);
+        BaseAdapter adapter = new BaseAdapter() {
+            ArrayList<String> lista = new ArrayList();
+
+            @Override
+            public int getCount() {
+                return lista.size();
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return lista.get(position);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                return null;
+            }
+        };
 
 
         ////////////////////// Object creation ///////////////////////
